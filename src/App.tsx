fe55887b7374ecb27fdb073abff90a97ee221bd8
Abd-i-AbbasAlamSaeedi <abdiabbasalamsaeedi@gmail.com>
2026-04-20 15:31:51 +0500

    Add missing React main entrypoint for root mounting

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import SpaceInvadersGame from './components/SpaceInvadersGame';
+
+export default function App() {
+  return (
+    <main className="min-h-screen bg-slate-900">
+      <SpaceInvadersGame />
+    </main>
+  );
+}
